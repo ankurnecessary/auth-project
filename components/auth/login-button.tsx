0e83c916937321cad2552ce16bc0051dcf780df2
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-09-23 05:12:45 +0930

    [login-button]: Created a login button functionality to **navigate to the login page**.

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: 'modal' | 'redirect';
+  asChild?: boolean;
+}
+
+function LoginButton({
+  children,
+  mode = 'redirect',
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  asChild,
+}: LoginButtonProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/auth/login');
+  };
+
+  if (mode === 'modal') {
+    return <span>TODO: Modal implementation</span>;
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+}
+
+export default LoginButton;
