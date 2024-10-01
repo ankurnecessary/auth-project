c98da61fbb8140f3b28a184d088a1ab490e0de4e
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-10-02 09:19:01 +0930

    [register]: Created `<RegisterForm />` and added it in the route `auth/register`.

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,131 @@
+'use client';
+
+import React, { useEffect, useState, useTransition } from 'react';
+import CardWrapper from './card-wrapper';
+
+import * as z from 'zod';
+import { registerSchema } from '@/schemas';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+import { register } from '@/actions/register';
+
+export const RegisterForm = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  // This ensures that the component only renders after it's mounted on the client
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      name: '',
+    },
+  });
+
+  const submitHandler = (values: z.infer<typeof registerSchema>) => {
+    startTransition(async () => {
+      const data = await register(values);
+      setError(data.error);
+      setSuccess(data.success);
+    });
+  };
+
+  // Only render the form once the component is mounted
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <CardWrapper
+      headerlabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(submitHandler)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="jhon.doe@example.com"
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="jhon.doe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Create an account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
