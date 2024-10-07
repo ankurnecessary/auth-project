351a2772c5a285d4265f74d7317db863da310d5d
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-10-07 15:39:06 +1030

    [user-registration][prisma]: Added a non-existant user in the user table of the database.

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { registerSchema } from '@/schemas';
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFieldset = registerSchema.safeParse(values);
@@ -11,6 +14,22 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
       error: 'Invalid fields!',
     };
   }
+
+  const { email, password, name } = validatedFieldset.data!;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const isExistingUser = await getUserByEmail(email);
+
+  if (isExistingUser) {
+    return { error: 'Email already exists!' };
+  }
+
+  await db.user.create({
+    data: { name, email, password: hashedPassword },
+  });
+
+  // TODO: Send verification token email
 
   return {
     success: 'Registration successful!',
