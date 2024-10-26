3d6e9b85b3f2c27c25ad8dd8b48d19b12f95dead
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-10-26 13:28:07 +1030

    [user-session]: Added user id in the user session upon login.

--- auth.ts
+++ auth.ts
@@ -9,6 +9,19 @@ export const {
   signOut,
   handlers: { GET, POST },
 } = NextAuth({
+  callbacks: {
+    jwt({ token, user, profile }) {
+      console.log({ token, user, profile });
+      return token;
+    },
+    session({ session, token }) {
+      if (!!session.user && !!token.sub) {
+        session.user.id = token.sub;
+      }
+      console.log({ session, token });
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db), // Using PrismaAdapter to interact with the database
   session: { strategy: 'jwt' }, // Using JWT for authentication purposes
   ...authConfig,
