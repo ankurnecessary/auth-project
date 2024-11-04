91e7221eb535938f57b8d3207176a1655fa04f2e
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-11-04 13:27:03 +1030

    [social-login-github]: Added github as login provider in our application.

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,17 @@
 import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Github from 'next-auth/providers/github';
 
 import { loginSchema } from '@/schemas';
 import { getUserByEmail } from './data/user';
 
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = loginSchema.safeParse(credentials);
