229e64def819283dd559b2f8447c51fc0ba31ea5
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-11-07 13:35:41 +1030

    [social-login-google]: Added google as login provider in our application.

--- auth.config.ts
+++ auth.config.ts
@@ -2,12 +2,16 @@ import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import Github from 'next-auth/providers/github';
-
+import Google from 'next-auth/providers/google';
 import { loginSchema } from '@/schemas';
 import { getUserByEmail } from './data/user';
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
