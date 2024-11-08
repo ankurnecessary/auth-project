19113c0da266a8ea2d9f3adcd330413ed36fa3ff
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-11-08 11:53:23 +1030

    [social-login]: Now `emailVerified` column in the `User` table inside the DB will get automatically populated with the date and time when a user has signed in / up first time using any of the available social login methods (google / github).

--- auth.ts
+++ auth.ts
@@ -53,6 +53,14 @@ export const {
   signOut,
   handlers: { GET, POST },
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     jwt,
     session,
