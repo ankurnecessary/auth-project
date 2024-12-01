738126220107d2946b4e9cdce5e1b1f3f08d2636
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-12-01 13:02:42 +1030

    [CI-error][resend-web-check] Introduced a check where `Resend` library will not work if it is being called form the browser.

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,9 +1,13 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend =
+  process.env.RESEND_API_KEY && typeof window === 'undefined'
+    ? new Resend(process.env.RESEND_API_KEY)
+    : null;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  if (!resend) return;
   await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: email,
